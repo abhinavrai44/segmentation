fdd6738e5c796b860fdcdc8f10ce58226a478505
Abhinav Rai <abhinavrai44@gmail.com>
2019-08-24 14:01:36 +0800

    VGG16 base network

---
+++ model/vgg16.py
@@ -0,0 +1,45 @@
+import keras
+import tensorflow as tf
+from keras.layers import Input, ZeroPadding2D, Conv2D, BatchNormalization, Activation, Add, MaxPooling2D, UpSampling2D
+from keras.regularizers import l2
+from keras.models import Model
+
+def vgg16_encoder(image_shape):
+	
+	image_input = Input(shape=image_shape)
+
+	#Block 1
+	x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(image_input)
+	x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+	f1 = x
+
+	# Block 2
+	x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+	x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+	f2 = x
+
+	# Block 3
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+	f3 = x
+
+	# Block 4
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+	f4 = x
+
+	# Block 5
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+	x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+	
+	f5 = x
+
+	return image_input, f1, f2, f3, f4, f5
